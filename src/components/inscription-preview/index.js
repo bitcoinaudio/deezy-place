9c9c0d22102b124b546c7f60a198f9b0461ef24e
bitcoinaudio <bitcoinaudio@gmail.com>
2024-11-27 22:06:53 -0500

    Update index.js

--- src/components/inscription-preview/index.js
+++ src/components/inscription-preview/index.js
@@ -4,6 +4,7 @@ import { isImageInscription } from "@services/nosft";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 
 let ORDINALS_EXPLORER_URL = "https://radinals.bitcoinaudio.co";
+//  let ORDINALS_EXPLORER_URL = "http://localhost";
 
 export const InscriptionPreview = ({ utxo }) => {
     const [loading, setLoading] = useState(true);
